430f2f1bcc17fb84781378192d06d5ad0c8c7fb1
hkha2006 <hkha2006@gmail.com>
2021-12-07 21:30:52 +0700

    UpdateCovidChart

---
+++ src/SERVER/Entity/InfomationCovid.java
@@ -0,0 +1,74 @@
+package SERVER.Entity;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.sql.Date;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import SERVER.Model.CovidInfoModel;
+
+public class InfomationCovid {
+	
+	public static void main(String[] args) {
+		
+		String country="Vietnam";
+		String minDate= "2021-08-01", maxDate="2021-08-05"; //dinh dang yyyy-mm-dd
+		
+//		try {
+//			ArrayList<CovidInfoModel> dataCovidOneDay = getDataCovid(country, minDate, maxDate);
+////			System.out.println(dataCovidOneDay.toString());
+//			for (CovidInfoModel covidInfor: dataCovidOneDay) {
+//				System.out.println(covidInfor.date);
+//				System.out.println("Số ca nhiễm: "+covidInfor.confirmed);
+//				System.out.println("Số ca tử vong: "+covidInfor.deaths);
+//				System.out.println("Số ca hồi phục: "+covidInfor.recovered+"\n");
+//			}
+//			
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+	}
+
+// lấy dữ liệu từ api theo quốc gia, ngày, và trả về ds Covid
+	public static ArrayList<CovidInfoModel> getDataCovid(String country, String minDate, String maxDate) throws IOException, InterruptedException {
+			
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create("https://webhooks.mongodb-stitch.com/api/client/v2.0/app/covid-19-qppza/service/REST-API/incoming_webhook/countries_summary?country=" + country +"&min_date=" + minDate + "T00:00:00.000Z&max_date="+ maxDate +"T00:00:00.000Z&hide_fields=_id,%20uids,%20country_iso2s,%20states,%20combined_names,%20country_codes,%20country_iso3s"))
+				.method("GET", HttpRequest.BodyPublishers.noBody())
+				.build();
+		HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+		
+		ArrayList<CovidInfoModel> dataCovid = new ArrayList<CovidInfoModel>();
+		CovidInfoModel data;
+		
+		JSONArray ar = new JSONArray(response.body());
+		for(int i = 0; i< ar.length()-1; i++) {
+			JSONObject oneDayData = ar.getJSONObject(i);
+			
+			int confirmed = oneDayData.getInt("confirmed");
+			int deaths = oneDayData.getInt("deaths");
+			int recovered = oneDayData.getInt("recovered");
+			int active =  confirmed - deaths - recovered;
+			String date = oneDayData.getString("date");
+			int population = oneDayData.getInt("population");
+			int confirmed_daily = oneDayData.getInt("confirmed_daily");
+			int deaths_daily = oneDayData.getInt("deaths_daily");
+			int recovered_daily = oneDayData.getInt("population");
+			
+			data = new CovidInfoModel(confirmed, deaths, recovered, active, date, population, confirmed_daily, deaths_daily, recovered_daily);
+			
+			dataCovid.add(data);
+		}	
+		return dataCovid;
+	}
+	
+	
+}
---
+++ src/SERVER/Model/CovidInfoModel.java
@@ -0,0 +1,87 @@
+package SERVER.Model;
+
+import java.io.Serializable;
+
+public class CovidInfoModel implements Serializable{
+
+	int confirmed;
+	int deaths;
+	int recovered;
+	int active;
+	String date;
+	int population;
+	int confirmed_daily;
+	int deaths_daily;
+	int recovered_daily;
+	public CovidInfoModel(int confirmed, int deaths, int recovered, int active, String date, int population,
+			int confirmed_daily, int deaths_daily, int recovered_daily) {
+		super();
+		this.confirmed = confirmed;
+		this.deaths = deaths;
+		this.recovered = recovered;
+		this.active = active;
+		this.date = date;
+		this.population = population;
+		this.confirmed_daily = confirmed_daily;
+		this.deaths_daily = deaths_daily;
+		this.recovered_daily = recovered_daily;
+	}
+	public int getConfirmed() {
+		return confirmed;
+	}
+	public void setConfirmed(int confirmed) {
+		this.confirmed = confirmed;
+	}
+	public int getDeaths() {
+		return deaths;
+	}
+	public void setDeaths(int deaths) {
+		this.deaths = deaths;
+	}
+	public int getRecovered() {
+		return recovered;
+	}
+	public void setRecovered(int recovered) {
+		this.recovered = recovered;
+	}
+	public int getActive() {
+		return active;
+	}
+	public void setActive(int active) {
+		this.active = active;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getPopulation() {
+		return population;
+	}
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+	public int getConfirmed_daily() {
+		return confirmed_daily;
+	}
+	public void setConfirmed_daily(int confirmed_daily) {
+		this.confirmed_daily = confirmed_daily;
+	}
+	public int getDeaths_daily() {
+		return deaths_daily;
+	}
+	public void setDeaths_daily(int deaths_daily) {
+		this.deaths_daily = deaths_daily;
+	}
+	public int getRecovered_daily() {
+		return recovered_daily;
+	}
+	public void setRecovered_daily(int recovered_daily) {
+		this.recovered_daily = recovered_daily;
+	}
+	
+	
+	
+	
+}
